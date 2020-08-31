23aeb2058d3ed55b3cc4dc762c53fbb0491d63d7
john-sousa96 <69728894+john-sousa96@users.noreply.github.com>
2020-08-31 12:47:14 -0300

    Create Form1.Designer.cs
    design login

---
+++ desktop/software_estacionamento/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace software_estacionamento
+{
+    partial class form_login
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_user_login = new System.Windows.Forms.Label();
+            this.lbl_senha_login = new System.Windows.Forms.Label();
+            this.txt_user = new System.Windows.Forms.TextBox();
+            this.txt_senha = new System.Windows.Forms.TextBox();
+            this.btn_acessar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_user_login
+            // 
+            this.lbl_user_login.AutoSize = true;
+            this.lbl_user_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_user_login.Location = new System.Drawing.Point(13, 41);
+            this.lbl_user_login.Name = "lbl_user_login";
+            this.lbl_user_login.Size = new System.Drawing.Size(87, 24);
+            this.lbl_user_login.TabIndex = 0;
+            this.lbl_user_login.Text = "Usuário:";
+            // 
+            // lbl_senha_login
+            // 
+            this.lbl_senha_login.AutoSize = true;
+            this.lbl_senha_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_senha_login.Location = new System.Drawing.Point(13, 102);
+            this.lbl_senha_login.Name = "lbl_senha_login";
+            this.lbl_senha_login.Size = new System.Drawing.Size(76, 24);
+            this.lbl_senha_login.TabIndex = 1;
+            this.lbl_senha_login.Text = "Senha:";
+            // 
+            // txt_user
+            // 
+            this.txt_user.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_user.Location = new System.Drawing.Point(95, 39);
+            this.txt_user.Name = "txt_user";
+            this.txt_user.Size = new System.Drawing.Size(198, 26);
+            this.txt_user.TabIndex = 2;
+            // 
+            // txt_senha
+            // 
+            this.txt_senha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_senha.Location = new System.Drawing.Point(95, 102);
+            this.txt_senha.Name = "txt_senha";
+            this.txt_senha.Size = new System.Drawing.Size(198, 26);
+            this.txt_senha.TabIndex = 3;
+            // 
+            // btn_acessar
+            // 
+            this.btn_acessar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_acessar.Location = new System.Drawing.Point(82, 144);
+            this.btn_acessar.Name = "btn_acessar";
+            this.btn_acessar.Size = new System.Drawing.Size(141, 48);
+            this.btn_acessar.TabIndex = 4;
+            this.btn_acessar.Text = "Acessar";
+            this.btn_acessar.UseVisualStyleBackColor = true;
+            // 
+            // form_login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(318, 213);
+            this.ControlBox = false;
+            this.Controls.Add(this.btn_acessar);
+            this.Controls.Add(this.txt_senha);
+            this.Controls.Add(this.txt_user);
+            this.Controls.Add(this.lbl_senha_login);
+            this.Controls.Add(this.lbl_user_login);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "form_login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.form_login_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_user_login;
+        private System.Windows.Forms.Label lbl_senha_login;
+        private System.Windows.Forms.TextBox txt_user;
+        private System.Windows.Forms.TextBox txt_senha;
+        private System.Windows.Forms.Button btn_acessar;
+    }
+}
+
