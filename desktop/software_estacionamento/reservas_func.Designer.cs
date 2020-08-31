834b48909c25016f02634d0a8ea2edc9175caa69
john-sousa96 <69728894+john-sousa96@users.noreply.github.com>
2020-08-31 12:43:28 -0300

    Create reservas_func.Designer.cs
    Design da tela de reserva funcionario

---
+++ desktop/software_estacionamento/reservas_func.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace software_estacionamento
+{
+    partial class reservas_func
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.id_reserva = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nome_usuario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.id_placa = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.servico = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.vaga = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.timestamp_inicio_uso = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.timestamp_final_uso = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.subtotal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Enabled = false;
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(87, 83);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(418, 31);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id_reserva,
+            this.nome_usuario,
+            this.id_placa,
+            this.servico,
+            this.vaga,
+            this.timestamp_inicio_uso,
+            this.timestamp_final_uso,
+            this.subtotal});
+            this.dataGridView1.Location = new System.Drawing.Point(87, 157);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1043, 508);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // id_reserva
+            // 
+            this.id_reserva.HeaderText = "Reserva";
+            this.id_reserva.Name = "id_reserva";
+            // 
+            // nome_usuario
+            // 
+            this.nome_usuario.FillWeight = 250F;
+            this.nome_usuario.HeaderText = "Nome cliente";
+            this.nome_usuario.Name = "nome_usuario";
+            this.nome_usuario.Width = 300;
+            // 
+            // id_placa
+            // 
+            this.id_placa.HeaderText = "Placa do veículo";
+            this.id_placa.Name = "id_placa";
+            // 
+            // servico
+            // 
+            this.servico.HeaderText = "Serviço";
+            this.servico.Name = "servico";
+            // 
+            // vaga
+            // 
+            this.vaga.HeaderText = "vaga";
+            this.vaga.Name = "vaga";
+            // 
+            // timestamp_inicio_uso
+            // 
+            this.timestamp_inicio_uso.HeaderText = "Hora inicial";
+            this.timestamp_inicio_uso.Name = "timestamp_inicio_uso";
+            // 
+            // timestamp_final_uso
+            // 
+            this.timestamp_final_uso.HeaderText = "hora final";
+            this.timestamp_final_uso.Name = "timestamp_final_uso";
+            // 
+            // subtotal
+            // 
+            this.subtotal.HeaderText = "Subtotal";
+            this.subtotal.Name = "subtotal";
+            // 
+            // reservas_func
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1202, 739);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "reservas_func";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Reservas";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id_reserva;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nome_usuario;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id_placa;
+        private System.Windows.Forms.DataGridViewTextBoxColumn servico;
+        private System.Windows.Forms.DataGridViewTextBoxColumn vaga;
+        private System.Windows.Forms.DataGridViewTextBoxColumn timestamp_inicio_uso;
+        private System.Windows.Forms.DataGridViewTextBoxColumn timestamp_final_uso;
+        private System.Windows.Forms.DataGridViewTextBoxColumn subtotal;
+    }
+}
